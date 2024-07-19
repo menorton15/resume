b6a6572f8bf3cfc408e868be0c351b506b6ce760
menorton15 <menorton15@hotmail.com>
2024-07-19 15:32:24 -0500

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import Image from "next/image";
 
 import Header from "../app/_components/Header";
 import Contact from "../app/_components/Contact";
@@ -20,14 +21,56 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-top">
-        <div className="container w-3/4 flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="justify-top flex min-h-screen flex-col items-center">
+        <div className="container flex w-3/4 flex-col items-center justify-center gap-12 px-4 py-16">
           <Header />
-          <CareerObjective />
-          <Education />
-          <Projects />
-
+          <div className="my-16 flex lg:flex-row sm:flex-col w-full rounded-full items-center">
+            <Image
+              className="rounded-full max-sm:w-100 max-sm:h-100"
+              src={"/profile.jpg"}
+              alt={"profile"}
+              width={500}
+              height={500}
+            />
+            <div className="lg:mx-16 flex w-full flex-col items-center text-white">
+              <h3 className="text-xl my-16 text-center">About Me</h3>
+              <p className="my-5">
+                Hi, I&apos;m Matthew Norton, a skilled Software Engineer with
+                extensive experience in software development. My
+                expertise spans across multiple programming languages and
+                technologies, including Python, Golang, C#, C++,
+                JavaScript, React, and various databases. I have a strong
+                background in developing web3 and blockchain platforms, internal tools,
+                and web applications.
+              </p>
+              <p className="my-5">
+                With a solid foundation in Software Engineering from Brigham
+                Young University Idaho, coupled with an Associate of Applied
+                Sciences in Business Administration from Provo College, I have
+                honed my technical skills and understanding of business
+                processes. I am passionate about solving complex problems and
+                continuously learning new technologies to stay ahead in the
+                ever-evolving tech landscape.
+              </p>
+              <p className="my-5">
+                Throughout my career, I have collaborated with talented teams at
+                Merkle Root, Goldman Sachs, and other organizations to deliver
+                high-quality software solutions. I have a proven track record of
+                improving efficiency, reducing costs, and enhancing user
+                experiences through innovative software engineering practices.
+              </p>
+              <p className="my-5">
+                When I&apos;m not coding, I enjoy exploring the latest advancements
+                in technology, contributing to open-source projects, and
+                engaging with the tech community. I am currently seeking a
+                remote role where I can leverage my diverse skill set to build
+                professional-grade applications and drive impactful results.
+              </p>
+              </div>
+          </div>
         </div>
+        <p className="my-5 mb-14 text-white"><a className="underline" href={"/skills"}>Click here</a> for a full list of my skills.</p>
+            
       </main>
     </HydrateClient>
   );
